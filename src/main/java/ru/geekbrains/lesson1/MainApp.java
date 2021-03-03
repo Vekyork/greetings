d602bb8104a7210ad741bd10c096a1d43cf4591d
Vekyork <vekyork@yandex.ru>
2021-03-03 16:16:03 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson1/MainApp.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson1;
+
+public class MainApp {
+    public static void main(String[] args) {
+        String name = "123";
+        System.out.println("Имя = " + name);
+    }
+}
